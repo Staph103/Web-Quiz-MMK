ca9de2984f026f66a88ce3904e86797c92c6bbbb
MMK <staph103@gmail.com>
2023-11-29 19:19:46 -0500

    Finally got the questions to render and hide button

--- script.js
+++ script.js
@@ -2,45 +2,75 @@
 
 var questionSlot = document.querySelector("#quiz-questions");
 var choicesClass = document.querySelector(".Choices");
-var nextButton = document.querySelector("#next");
+var buttonStyle = document.querySelector(".choicesButton");
+var nextButton = document.getElementById("next");
+var startButton = document.getElementById("start");
+var backgroundImg = document.querySelector(".back-img");
+var gameTitle = document.querySelector("#game-title");
+var countDown = document.getElementById("Count-Down");
+var yourFinalScore = document.querySelector(".yourscore");
 
 var score = 0;
 var index = 0;
 
+let secondsLeft = 60
 
-
-
-questionSlot.setAttribute("style","font-size: 40px; color: red; border-bottom: 5px solid red; font-weight: 700; padding-bottom: 20px; margin-bottom: 50px");
-choicesClass.setAttribute("style","font-size: 45px");
+countDown.setAttribute("style", "font-size: 30px; color: white; text-align:right; margin:20px; font-weight: 700")
+questionSlot.setAttribute("style","font-size: 40px; color: rgb(1, 1, 121); border-bottom: 5px solid rgb(1, 1, 121); font-weight: 700; padding-bottom: 20px; margin-bottom: 50px");
+// buttonStyle.setAttribute("style","font-size: 30px");
 
 var quiz = [
     {
         question: "Who created this game",
-        choices: ["Andrew", "Diego", "Mustapha", "Alisa"],
-        rightAnswer: "Mustapha",
-    },
-    {
-        question: "What programing language allows you to implement complex features on web pages?",
-        choices: ["HTML", "CSS", "JavaScript", "Python"],
-        rightAnswer: "JavaScript",
-    },
-    {
-        question: "Who created this game",
-        choices: ["Andrew", "Diego", "Mustapha", "Alisa"],
-        rightAnswer: "Mustapha",
-    },
-    {
-        question: "Who created this game",
-        choices: ["Andrew", "Diego", "Mustapha", "Alisa"],
-        rightAnswer: "Mustapha",
-    },
-    {
-        question: "Who created this game",
-        choices: ["Andrew", "Diego", "Mustapha", "Alisa"],
-        rightAnswer: "Mustapha",
-    }
+        answers: [
+            {choice: "Andrew", correct: false},
+            {choice: "Diego", correct: false},
+            {choice: "Mustapha", correct: true},
+            {choice: "Alisa", correct: false},
+        ]
 
-];
+    },
+    {
+        question: "How tall is Mustapha",
+        answers: [
+            {choice: "6'3", correct: true},
+            {choice: "5'9", correct: false},
+            {choice: "4'11", correct: false},
+            {choice: "6'7", correct: false},
+        ]
+
+    },
+    {
+        question: "Where am is Mustapha from",
+        answers: [
+            {choice: "Spain", correct: false},
+            {choice: "Haiti", correct: false},
+            {choice: "France", correct: false},
+            {choice: "Africa", correct: true},
+        ]
+
+    },
+    {
+        question: "Who created this game",
+        answers: [
+            {choice: "Andrew", correct: false},
+            {choice: "Diego", correct: false},
+            {choice: "Mustapha", correct: true},
+            {choice: "Alisa", correct: false},
+        ]
+
+    },
+    {
+        question: "Who created this game",
+        answers: [
+            {choice: "Andrew", correct: false},
+            {choice: "Diego", correct: false},
+            {choice: "Mustapha", correct: true},
+            {choice: "Alisa", correct: false},
+        ]
+
+    }
+]
 
 
 
@@ -53,72 +83,153 @@ var quiz = [
 
 
 
-var backgroundImg = document.querySelector(".back-img");
 document.body.style.backgroundImage = "url('startscreen.webp')";
-var gameTitle = document.querySelector("#game-title");
 gameTitle.textContent= "Web API Quiz";
-var startButton = document.querySelector("#start");
-startButton.addEventListener("click",startGame());
+
+
 
 function startGame(){
     index = 0;
     score = 0;
+    var secondsLeft = 60;
+    startButton.classList.add('hide');
+    questionSlot.classList.remove('hide');
+    choicesClass.classList.remove("hide");
+    gameTitle.classList.add("hide")
 
-    displayQuestion();
-
-}
-function displayQuestion(){
-    var currentQuestion = quiz[index];
-    var questionNum = index + 1;
-    questionSlot.innerHTML = questionNum + ".)" + currentQuestion.question;
     
-    currentQuestion.choices.forEach(choice => {
+    
+    setTime()
+    
+    displayQuestion();
+    
+}
+
+
+
+function displayQuestion(options){
+    renderBack();
+    currentQuestion = quiz[index];
+    questionSlot.innerText = currentQuestion.question;
+    
+    currentQuestion.answers.forEach(option => {
         var button = document.createElement("button")
-        button.innerHTML = currentQuestion.choices[index];
-        index++;
-        // choicesClass.appendChild(button)
+        button.innerText = option.choice;
         button.classList.add("choicesButton");
         choicesClass.appendChild(button);
         
+        button.setAttribute("data-correct",option.correct)
+        
+        
+        button.addEventListener("click",rightAnswer);
+        
+        
     })
 }
-startGame();
+function nextQuestion(){
+    // renderBack();
+    
+    
+}
+function renderBack(){
+    
+    nextButton.classList.add("hide")
+    while(choicesClass.firstChild){
+        choicesClass.removeChild(choicesClass.firstChild)
+    }
+    
+}
+function rightAnswer(event){
+    
+    var yourSmart = event.target;
+    console.log(yourSmart);
+    var right = yourSmart.dataset.correct;
+    console.log(right);
+    if (right === "true") {
+        yourSmart.classList.add("Thats-right");
+        score++;
+    }else{
+        
+        yourSmart.classList.add("Thats-wrong");
+        
+    }
+    Array.from(choicesClass.children).forEach(button =>{
+        if(button.dataset.correct === "true"){
+            button.classList.add("Thats-right");
+        }
+        button.disabled = true;
+    });
+    nextButton.classList.remove("hide");
+    
+    console.log(right);
+}
+function nextButtonFun(){
+    console.log("hit");
+    nextButton.classList.add('hide');
+    if(index < quiz.length){
+        loadNext();
+    }
+    
+    
+}
+function loadNext(){
+    index++;
+    if(index < quiz.length){
+        displayQuestion();
+    }else{
+        questionSlot.classList.add('hide');
+        choicesClass.classList.add('hide');
+        yourScore();
+    }
+}
+function yourScore(){
+    
+    yourFinalScore.classList.remove("hide");
+    document.getElementById("score").textContent = score;
+    document.getElementById('submit').onclick = saveScore;
+
+}
+
+function saveScore(){
+var highScores = JSON.parse(localStorage.getItem("scores")) || [];
+var newScore = {
+    initials: document.getElementById("ranking").value,
+    score: score,
+}
+highScores.push(newScore);
+localStorage.setItem("scores",JSON.stringify(highScores));
+}
+
+function setTime() {
+    // Sets interval in variable
+    var gameTime = setInterval(function() {
+        secondsLeft--;
+        countDown.textContent = secondsLeft ;
+        
+        if(secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(gameTime);
+            // Calls function to create and append image
+        }
+        
+    }, 1000);
+}
+startButton.addEventListener("click",startGame);
+nextButton.addEventListener("click",nextButtonFun);
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// function renderBack(){
+    //     nextButton.style.display = 'none';
+    //     while
+    // }
+    
+    // startGame();
+    
+    
 
 
 
@@ -192,29 +303,6 @@ startGame();
     
     
     
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
